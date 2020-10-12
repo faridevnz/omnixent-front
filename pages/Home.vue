d013c10f3198cdd90b300df6647e5f16e9368b8e
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2020-10-12 16:40:08 +0200

    added TabSelector to Home and tabSelector handler

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,9 +1,13 @@
 <template>
-  <BaseInputWithButton width="50" height="100" @submit="startSearch">
-    <BaseIcon width="41" height="21" view-box="0 0 41 21">
-      <RightArrow />
-    </BaseIcon>
-  </BaseInputWithButton>
+  <div>
+    <BaseInputWithButton width="50" height="100" @submit="startSearch">
+      <BaseIcon width="41" height="21" view-box="0 0 41 21">
+        <RightArrow />
+      </BaseIcon>
+    </BaseInputWithButton>
+    <br>
+    <BaseTabSelector width="21" :tab-items="availableServices" @tabChange="changeService" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,6 +17,8 @@ export default {
   components: { RightArrow },
   data () {
     return {
+      availableServices: ['Google', 'YouTube', 'Amazon'],
+      selectedService: 'Google',
       query: ''
     }
   },
@@ -20,6 +26,9 @@ export default {
     startSearch (query: string): void {
       // search method
       this.query = query
+    },
+    changeService (newServiceIndex: number): void {
+      this.selectedService = this.availableServices[newServiceIndex]
     }
   }
 }
